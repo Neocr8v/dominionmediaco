0308edc887786bd47353f3369ee53d2a14fc3986
Neocr8v <neocr8vmedia@gmail.com>
2025-12-12 08:13:13 +0000

    new

--- src/components/home/ImageShowcase.tsx
+++ src/components/home/ImageShowcase.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 const images = [
   { id: 1, src: '/images/Wedding-025.jpg', alt: 'Image 1' },
-  { id: 2, src: '/images/placeholder2.jpg', alt: 'Placeholder Image 2' },
+  { id: 2, src: '/images/Wedding-089.jpg', alt: 'Image 2' },
   { id: 3, src: '/images/placeholder3.jpg', alt: 'Placeholder Image 3' },
   { id: 4, src: '/images/placeholder4.jpg', alt: 'Placeholder Image 4' },
   { id: 5, src: '/images/placeholder5.jpg', alt: 'Placeholder Image 5' },
