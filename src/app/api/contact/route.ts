22ea720678bc58a669dd862db34f9a97c2ad63f6
Neocr8v <neocr8vmedia@gmail.com>
2026-01-13 16:08:55 +0000

    Update route.ts

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,9 +1,7 @@
 import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 
-export async function GET(request: Request) {
-  return NextResponse.json({ message: 'GET request successful' }, { status: 200 });
-}
+export const dynamic = 'force-dynamic';
 
 export async function POST(request: Request) {
   try {
