3c84071d99562e8f3b1dd56350de80692e4f8d23
Neocr8v <neocr8vmedia@gmail.com>
2026-01-14 16:47:37 +0000

    Update sanity.ts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,11 +2,11 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: 'f6km7cno',
-  dataset: 'production', // or your dataset name
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: '2024-07-16', // use a UTC date in YYYY-MM-DD format
   useCdn: true, // `false` if you want to ensure fresh data
-  // token: 'YOUR_READ_ONLY_API_TOKEN', // uncomment and add your token if you have one
+  token: process.env.SANITY_API_TOKEN, // or leave blank for public datasets
 });
 
 const builder = imageUrlBuilder(client);
